6a466cb025386944b6ff212b8d9554b10db1f82b
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-24 03:36:44 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,9 +20,10 @@ function pressIt() {
 }
 
 function submitIt() {
-  
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
-
 });
